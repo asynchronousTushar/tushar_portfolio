8d25254e0b340fcbc70c7e37ec953f69d1abaccf
Tushar Biswas <tusarbiswas888@gmail.com>
2022-07-09 15:32:08 +0600

    NavBar component design complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,44 @@
 import './Navbar.css';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [activeNav, setActiveNav] = useState("#")
     return (
-        <div>
-            Navbar
-        </div>
+        <nav>
+            <a href="#"
+                onClick={() => setActiveNav("#")}
+                className={activeNav === "#" ? "active" : ""}
+            >
+                <AiOutlineHome />
+            </a>
+            <a href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={activeNav === "#about" ? "active" : ""}
+            >
+                <AiOutlineUser />
+            </a>
+            <a href="#experience"
+                onClick={() => setActiveNav("#experience")}
+                className={activeNav === "#experience" ? "active" : ""}
+            >
+                < BiBook />
+            </a>
+            <a href="#services"
+                onClick={() => setActiveNav("#services")}
+                className={activeNav === "#services" ? "active" : ""}
+            >
+                <RiServiceLine />
+            </a>
+            <a href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={activeNav === "#contact" ? "active" : ""}
+            >
+                <BiMessageSquareDetail />
+            </a>
+        </nav>
     );
 }
 
